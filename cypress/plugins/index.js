be55b037bdf46269ae91a192fd2d8147bbb0bb23
최종호 <victor_77@naver.comgit config --global user.name emtete>
2020-06-22 16:01:03 +0900

    add setting cypress-react-unit-test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,6 +16,8 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+  require("cypress-react-unit-test/plugins/cra-v3")(on, config);
+  // IMPORTANT to return the config object
+  // with the any changed environment variables
+  return config;
+};
