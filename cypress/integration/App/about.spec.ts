dcf6d4b86950ac1f0ae7ff9724988fb9d9ab4dd6
최종호 <victor_77@naver.comgit config --global user.name emtete>
2020-06-23 11:56:12 +0900

    Navbar component update

---
+++ cypress/integration/App/about.spec.ts
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+import * as About from "/Users/victor/dev/portfolio/src/components/About";
+
+describe("App", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("check getListElement", () => {
+    cy.document().then((doc) => {
+      const lis = About.getListElement(doc.documentElement);
+      expect(lis.length).to.equal(5);
+    });
+  });
+
+  it("check setActive", () => {
+    cy.get(".about_about__content__3Zl7G li:nth-child(2)").click();
+    cy.document().then((doc) => {
+      const li = doc.documentElement.querySelector(".about_about__content__3Zl7G li:nth-child(2)");
+      const clas = li.getAttribute("class");
+      expect(clas).to.contain("about_active__1IkPv");
+    });
+  });
+});
+
+// cy.get("#work_work__3-S2O").scrollIntoView({ easing: "linear" });
+
+// function getSectionElement(doc) {
+//   const root = doc.documentElement.querySelector("#root");
+//   const section = About.getSectionsElement(root);
+//   return section;
+// }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,15 +2,37 @@ import React from "react";
 import style from "../style/about.module.css";
 
 class About extends React.Component {
+  private aboutRef = React.createRef<HTMLDivElement>();
+  componentDidMount() {
+    const rootElement = this.getElement();
+    const listElements = getListElement(rootElement);
+
+    // click 이벤트 바인딩
+    listElements.forEach((element) => {
+      element.addEventListener("click", (event) => {
+        setActive(event.target as Element, listElements);
+      });
+    });
+  }
+
+  getElement = (): HTMLDivElement => {
+    const about = this.aboutRef.current;
+    if (about) {
+      return about;
+    } else {
+      throw new Error("about is null");
+    }
+  };
+
   render() {
     return (
-      <section id={`${style.about}`} className={`${style.flex_column_center}`}>
+      <section id={`${style.about}`} className={`${style.flex_column_center}`} ref={this.aboutRef}>
         <h1 className={`${style.about__h1}`}>About Me</h1>
-        <p>hi, 저는 현재 서울 관악구 신림동에 거주중이며,...</p>
+        <p>현재 서울 관악구 신림동에 거주중입니다.</p>
         <div className={`${style.about__content}`}>
           <div className={`${style.about__menu__warp}`}>
             <ul className={`${style.about__menu}`}>
-              <li>2008 ~ 2009 : 군 입대sssss</li>
+              <li className={`${style.active}`}>2008 ~ 2009 : 군 입대sssss</li>
               <li>2008 ~ 2009 : 군 입대</li>
               <li>2008 ~ 2009 : 군 입대</li>
               <li>2008 ~ 2009 : 군 입대</li>
@@ -30,5 +52,32 @@ class About extends React.Component {
     );
   }
 }
+
+/**
+ * * setActive
+ * * : 매개변수로 받은 element을 활성화시킨다.
+ * @param target
+ * @param liElement
+ * @return void
+ */
+export const setActive = (target: Element, listElements: NodeListOf<Element>): void => {
+  listElements.forEach((element) => {
+    element.classList.remove(`${style.active}`);
+  });
+  target.classList.add(`${style.active}`);
+};
+
+/**
+ * * getListElement
+ * * : li Element를 담은 Html Element를 반환한다.
+ * @param element
+ * @return Elements
+ */
+export const getListElement = (element: Element): NodeListOf<Element> => {
+  const lis = element.querySelectorAll(".about_about__content__3Zl7G li");
+  if (!lis) throw new Error("Called before rendering");
+
+  return lis;
+};
 
 export default About;
