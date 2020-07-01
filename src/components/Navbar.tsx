d8d09e5c0ee013a0b7c232ab061ed2f1e63187df
최종호 <victor_77@naver.com>
2020-07-01 15:50:30 +0900

    update to responsive, Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ class Navbar extends React.Component<NavProps, NavState> {
     return (
       <nav id='navbar' className='notFixed'>
         <div className='navbar__menu'>
-          <ul>
+          <ul className='navbar__text'>
             <li id='nav__home' className='nav__btn active'>
               Home
             </li>
@@ -30,6 +30,24 @@ class Navbar extends React.Component<NavProps, NavState> {
             </li>
             <li id='nav__contact' className='nav__btn'>
               Contact
+            </li>
+          </ul>
+
+          <ul className='navbar__dot'>
+            <li id='nav__home' className='nav__btn active'>
+              H
+            </li>
+            <li id='nav__about' className='nav__btn'>
+              A
+            </li>
+            <li id='nav__skills' className='nav__btn'>
+              S
+            </li>
+            <li id='nav__work' className='nav__btn'>
+              W
+            </li>
+            <li id='nav__contact' className='nav__btn'>
+              C
             </li>
           </ul>
         </div>
