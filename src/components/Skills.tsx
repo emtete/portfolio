328a2ed0b18cc0e4ba173e42cc658c7c73d34a6d
최종호 <victor_77@naver.com>
2020-07-02 15:50:21 +0900

    (refectoring) skills component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,36 +4,9 @@ import ProgressBar from "./ProgressBar";
 import "../style/skills.scss";
 
 class Skills extends React.Component {
-  private skillsRef = React.createRef<HTMLDivElement>();
-
-  componentDidMount() {
-    const rootElement = this.getElement();
-    const spans = getTitleElements(rootElement);
-
-    spans.forEach((element) => {
-      element.addEventListener("click", (event) => {
-        setActive(event.target as Element, spans);
-      });
-    });
-  }
-
-  /**
-   * * getElement
-   * * : Skills의 root Element를 반환한다.
-   * @return HTMLDivElement
-   */
-  getElement = (): HTMLDivElement => {
-    const skills = this.skillsRef.current;
-    if (skills) {
-      return skills;
-    } else {
-      throw new Error("skills is null");
-    }
-  };
-
   render() {
     return (
-      <section id='skills' ref={this.skillsRef}>
+      <section id='skills'>
         <h1 className='skills__h1'>Skills</h1>
         <div className='hex_wrap3'>
           <div className='hex_wrap2'>
@@ -89,31 +62,4 @@ class Skills extends React.Component {
     );
   }
 }
-
-/**
- * * getTitleElements
- * * : Progressbar의 title span Element들을 반환한다.
- * @param element
- * @return Element
- */
-export const getTitleElements = (element: Element): NodeListOf<Element> => {
-  const span = element.querySelectorAll(".skills__description span:nth-child(1)");
-  if (!span) throw new Error("Called before rendering");
-  return span;
-};
-
-/**
- * * setActive
- * * : 매개변수로 받은 section을 활성화시킨다.
- * @param target
- * @param nodeList
- * @return void
- */
-export const setActive = (target: Element, nodeList: NodeListOf<Element>): void => {
-  nodeList.forEach((element) => {
-    element.classList.remove("active");
-  });
-  target.classList.add("active");
-};
-
 export default Skills;
